7560366b1c5aea8ac160dd98d4eee5dbfe087e92
laitzheng <75247468+laitzheng@users.noreply.github.com>
2021-02-23 13:46:28 +0800

    Add files via upload

---
+++ Bk_Manager.py
@@ -0,0 +1,40 @@
+import DB_Connect as connect  
+
+connection = connect.db.connect()
+
+def result(stmt):
+    return connection.execute(stmt).fetchall()[0][0]
+
+def ExtendBorrowing(bookID, memberID):
+    borrowerID = result("SELECT borrowMemberID FROM book WHERE bookID = {}".format(bookID))
+    reserverID = result("SELECT reserveMemberID FROM book WHERE bookID = {}".format(bookID))
+    if borrowerID == memberID and reserverID == None:
+        update_stmt = "UPDATE book SET dateDue = DATE_ADD(dateDue, INTERVAL 4 WEEK) WHERE bookID = {}".format(bookID)
+        connection.execute(update_stmt)
+        newDueDate = result('SELECT dateDue FROM book WHERE bookID = {}'.format(bookID))
+        return "Your due date has been extended by 4 weeks to {}.".format(newDueDate)
+    else:
+        return "You have not borrowed this book."
+
+def CancelReservation(bookID, memberID):
+    borrowerID = result("SELECT borrowMemberID FROM book WHERE bookID = {}".format(bookID))
+    reserverID = result("SELECT reserveMemberID FROM book WHERE bookID = {}".format(bookID))
+    if reserverID == memberID:
+        update_stmt = "UPDATE book SET reserveMemberID = NULL WHERE bookID = {}".format(bookID)
+        connection.execute(update_stmt)
+        if borrowerID == None:
+            response = input("The book is available for borrowing. Would you like to borrow?\n")
+            if response == "Yes":
+                return "XXX" #link up with Borrowing function 
+        return "Your reservation has been cancelled."
+    return "You do not have a reservation for this book."
+
+def ReturnBook(bookID, memberID):
+    borrowerID = result("SELECT borrowMemberID FROM book WHERE bookID = {}".format(bookID))
+    if borrowerID == memberID:
+        update_stmt = "UPDATE book SET borrowMemberID = NULL, dateDue = NULL WHERE bookID = {}".format(bookID)
+        connection.execute(update_stmt)
+        returnedBook = result("SELECT bookTitle FROM book WHERE bookID = {}".format(bookID))
+        return "{} is returned.".format(returnedBook)
+    else:
+        return "You have not borrowed this book."
